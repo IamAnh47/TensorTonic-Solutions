f0f5d2111e9a77d2f56717be2457fe89f5fca19c
Anh <119580911+IamAnh47@users.noreply.github.com>
2026-03-16 15:34:30 +0700

    Update solution for text-chunking

--- text-chunking/text-chunking.py
+++ text-chunking/text-chunking.py
@@ -6,14 +6,14 @@ def text_chunking(tokens, chunk_size, overlap):
         raise ValueError("Error: chunk size need bigger than overlap")
     if chunk_size <= 0:
         raise ValueError("Error: chuck size <= 0")
+    len_tokens = len(tokens)
+    if len_tokens <= chunk_size:
+        return [] if len_tokens == 0 else [tokens]
     chunks = []
     step = chunk_size - overlap
-    if len(tokens) == 0:
-        return []
-    if len(tokens) <= chunk_size:
-        return [tokens]
-    for i in range(0, len(tokens), step):
-        chunk = tokens[i: i + chunk_size]
-        if len(chunk) == chunk_size:
-            chunks.append(chunk)
+    chunks = [
+        tokens[i: i + chunk_size] 
+        for i in range(0, len_tokens, step) 
+        if len(tokens[i: i + chunk_size]) == chunk_size
+    ]
     return chunks
